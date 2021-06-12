60f36568f4acd4f6922b90bc33ad0127389a05f2
Harsh Tripathi <68777104+codeplinth@users.noreply.github.com>
2021-06-12 20:20:24 +0530

    Update read_from_aws.py

--- read_from_aws.py
+++ read_from_aws.py
@@ -5,8 +5,8 @@ spark = (
         .appName("schema_test")
         # .config("spark.jars.packages","com.amazonaws:aws-java-sdk:1.11.1026")
         # .config("spark.jars.packages","org.apache.hadoop:hadoop-aws:2.7.4")
-        # .config('fs.s3a.access.key',"AKIAUYWYBTX76YGDLX45")
-        # .config('fs.s3a.secret.key', "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")
+        # .config('fs.s3a.access.key',"xxxxxxxxxxxxxx")
+        # .config('fs.s3a.secret.key', "xxxxxxxxxxxx")
         .getOrCreate()
 )
 
@@ -19,8 +19,8 @@ spark.stop()
 
 #hadoop_conf=spark.sparkContext._jsc.hadoopConfiguration()
 
-#hadoop_conf.set("fs.s3a.access.key", "AKIAUYWYBTX73AZOJQGB")
-#hadoop_conf.set("fs.s3a.secret.key", "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")
+#hadoop_conf.set("fs.s3a.access.key", "xxxxxxxxxxxxxx")
+#hadoop_conf.set("fs.s3a.secret.key", "xxxxxxxxxxxxx")
 #hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 #hadoop_conf.set("com.amazonaws.services.s3.enableV4", "true")
 #hadoop_conf.set("fs.s3a.endpoint", "s3.us-east-2.amazonaws.com")
